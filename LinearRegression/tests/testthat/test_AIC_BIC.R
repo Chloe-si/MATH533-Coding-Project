7d4ed1852faa9272d9148719c88351f136d4e6de
sichuqiao <sichuqiao2002@gmail.com>
2024-12-11 18:47:33 -0500

    test for AIC/BIC. Update .Rd files

--- LinearRegression/tests/testthat/test_AIC_BIC.R
+++ LinearRegression/tests/testthat/test_AIC_BIC.R
@@ -1,52 +1,38 @@
-# File: tests/testthat/test_AIC_BIC.R
+#  test_file("tests/testthat/test_AIC_BIC.R")
 library(LinearRegression)
 
-#Example uses the iris dataset, model is Sepal.Length ~ Sepal.Width
 
-# test_that("AIC_BIC works correctly", {
-#   model <- lm(Sepal.Length ~ Sepal.Width, data = iris)
-#   result <- AIC_BIC(model)
-#
-#   expect_named(result, c("AIC", "BIC"))
-#   expect_true(is.numeric(result$AIC))
-#   expect_true(is.numeric(result$BIC))
-# })
+# Fit candidate models
+X1 = as.matrix(cbind(1, iris$Sepal.Width, iris$Petal.Length))
+X2 = as.matrix(cbind(1, iris$Sepal.Width, iris$Petal.Width))
+X3 = as.matrix(cbind(1, iris$Sepal.Width, iris$Petal.Length, iris$Petal.Width))
+y = iris$Sepal.Length
+ols_model1 = ols_fit(X1, y)
+ols_model2 = ols_fit(X2, y)
+ols_model3 = ols_fit(X3, y)
+ols_models <- list(ols_model1, ols_model2, ols_model3)
+
+lm_model1 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length, data = iris)
+lm_model2 <- lm(Sepal.Length ~ Sepal.Width + Petal.Width, data = iris)
+lm_model3 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width, data = iris)
+lm_models <- list(lm_model1, lm_model2, lm_model3)
 
 
+# Test that both implementations select the same model
+test_that("AIC/BIC select the correct model", {
+  lm_aic <- sapply(lm_models, AIC)
+  lm_bic <- sapply(lm_models, BIC)
+  lm_aic_best <- which.min(lm_aic)
+  lm_bic_best <- which.min(lm_bic)
 
+  ols_aic_bic <- sapply(ols_models, AIC_BIC)
+  ols_aic = as.numeric(ols_aic_bic[1,])
+  ols_bic = as.numeric(ols_aic_bic[2,])
+  ols_aic_best = which.min(ols_aic)
+  ols_bic_best = which.min(ols_bic)
 
-test_that("AIC and BIC are calculated correctly using ols_fit", {
-  # Prepare the iris dataset
-  data(iris)
-  X <- as.matrix(cbind(1, iris$Sepal.Width, iris$Petal.Length))  # Design matrix with intercept
-  y <- iris$Sepal.Length  # Response variable
-
-  # Fit the model using ols_fit
-  fitted_ols <- ols_fit(X, y)
-
-  # Fit the model using lm for comparison
-  fitted_lm <- lm(Sepal.Length ~ Sepal.Width + Petal.Length, data = iris)
-
-  # Calculate AIC and BIC using the custom function
-  aic_bic_ols <- AIC_BIC(fitted_ols)
-
-  # Calculate AIC and BIC using R's built-in functions
-  aic_lm <- AIC(fitted_lm)
-  bic_lm <- BIC(fitted_lm)
-
-  # Compare AIC values
-  expect_equal(
-    round(aic_bic_ols$AIC, 4),
-    round(aic_lm, 4),
-    tolerance = 1e-4,
-    info = "AIC values do not match between custom and R's lm implementation."
-  )
-
-  # Compare BIC values
-  expect_equal(
-    round(aic_bic_ols$BIC, 4),
-    round(bic_lm, 4),
-    tolerance = 1e-4,
-    info = "BIC values do not match between custom and R's lm implementation."
-  )
+  expect_equal(ols_aic_best, lm_aic_best)
+  expect_equal(ols_bic_best, lm_bic_best)
 })
+
+
