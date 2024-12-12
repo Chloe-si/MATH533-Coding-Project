bac408a83348a14aa62f1bcfe53f14b8403dcd99
Yuka Kato <145236040+Yuka-Kato-10181212@users.noreply.github.com>
2024-12-11 23:17:36 -0500

    Update ridge_estimator.R

--- LinearRegression/R/ridge_estimator.R
+++ LinearRegression/R/ridge_estimator.R
@@ -7,19 +7,19 @@
 #' @param lambda A numeric value representing the regularization parameter (\(\lambda > 0\)).
 #' @return A numeric vector containing the ridge regression coefficients.
 #' @examples
-#' set.seed(123)
-#' X <- matrix(rnorm(100), nrow = 10, ncol = 10)
-#' y <- rnorm(10)
-#' lambda <- 0.1
-#' ridge_estimator(X, y, lambda)
+#'X_values <- as.matrix(cbind(1, iris$Sepal.Width, iris$Petal.Width))
+#'colnames(X_values) <- c("Intercept", "Sepal.Width", "Petal.Width")
+#'y_values <- iris$Sepal.Length
+#'lambda <- 0.1
+#' ridge_estimator(X_values, y_values, lambda)
 #' @export
 #' 
 
 
-ridge_estimator <- function(X, y, lambda) {
+ridge_estimator <- function(X_values, y_values, lambda) {
   if (!is.matrix(X)) stop("Error: X must be a matrix.")
   if (!is.vector(y)) stop("Error: y must be a vector.")
-  if (lambda <= 0 || qr(X)$rank < ncol(X)) {
+  if (lambda < 0 || qr(X)$rank < ncol(X)) {
     stop("Error: Either lambda must be greater than zero or the design matrix X must be full rank.")
   }
   
@@ -31,4 +31,5 @@ ridge_estimator <- function(X, y, lambda) {
   
   return(ridge_beta)
 }
+
 
